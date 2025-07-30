9e5b741f7f973f358d1c4cefad331e3a22fbf291
magasinc <magasinc@espol.edu.ec>
2025-07-30 14:23:02 -0500

    Commit 1 metodo

--- calculatorsmp/src/test/java/calculatorsmp/OperationsTest.java
+++ calculatorsmp/src/test/java/calculatorsmp/OperationsTest.java
@@ -6,6 +6,7 @@ package calculatorsmp;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -63,6 +64,15 @@ public class OperationsTest {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
+    }
+    @Test
+    @DisplayName("Prueba de construccion sin datos null")
+    public void testMakeFormula_no_null() {
+        String formula = "";
+        Assertions.assertNotNull(formula);
+        Assertions.assertFalse(formula.isEmpty());
+        
+        
     }
     
 }
